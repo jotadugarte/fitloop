cd0d8e7f61cbdf608243a786c04c82f30b7aca7a
jotadugarte <jadere@gmail.com>
2026-05-31 16:15:58 -0600

    Step 17: Add routes, controllers, views, and enable analytics dashboard card

---
+++ app/controllers/admin/analytics_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Admin
+  class AnalyticsController < Admin::BaseController
+    def index
+      # Skeleton for now
+    end
+  end
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Admin
+  class UsersController < Admin::BaseController
+    def index
+      # Skeleton for now
+    end
+
+    def show
+      # Skeleton for now
+    end
+  end
+end
