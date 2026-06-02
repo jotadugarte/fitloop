02acc72183f77e1d068e1a157f201a809f48a48c
jotadugarte <jadere@gmail.com>
2026-06-01 21:59:03 -0600

    chore: remove OmniAuth devise modules and class methods from User model (B3)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@
 # ActiveRecord generates admin? automatically. Never set via user registration flow.
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :validatable, :confirmable,
-         :omniauthable, omniauth_providers: %i[google_oauth2 facebook apple]
+         :recoverable, :validatable, :confirmable
 
   alias_attribute :email_confirmed_at, :confirmed_at
 
@@ -38,24 +37,7 @@ class User < ApplicationRecord
     Subscription.active_at(at).exists?(user_id: id)
   end
 
-  def self.from_omniauth(auth, time_zone: nil)
-    self.precondition!(auth.info.email.present?)
 
-    user = find_or_initialize_by(provider: auth.provider.to_s, uid: auth.uid.to_s)
-    user.email = auth.info.email
-    user.name = auth.info.name.presence || user.email.to_s.split("@").first
-    user.time_zone = time_zone.presence || "America/Costa_Rica"
-    user.terms_accepted_at ||= Time.current
-    user.terms_version ||= TermsVersion.current
-    user.password = Devise.friendly_token[32] if user.new_record?
-    user.confirmed_at ||= Time.current if trusted_oauth_provider?(auth.provider)
-    user.save!
-    user
-  end
-
-  def self.trusted_oauth_provider?(provider)
-    %w[google_oauth2 facebook apple].include?(provider.to_s)
-  end
 
   private
 
